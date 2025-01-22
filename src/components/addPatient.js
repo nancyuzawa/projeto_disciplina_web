282274b2a529d95f51e4d843dbd54741c60b1c74
Nancy Yuzawa <110066948+nancyuzawa@users.noreply.github.com>
2025-01-22 18:27:55 +0000

    update

--- src/components/addPatient.js
+++ src/components/addPatient.js
@@ -1,86 +1,130 @@
-import '../css/global.css';
-import '../css/addPatient.css';
-import { useState } from 'react';
+import "../css/global.css";
+import "../css/addPatient.css";
+import { useState } from "react";
 
-function AddPatient({onClose, onAddPatient }){
-    const [name, setName] = useState('');    
-    const [peso, setPeso] = useState('');    
-    const [altura, setAltura] = useState('');    
-    const [gordura, setGordura] = useState('');    
-    const [imc, setImc] = useState('');    
+function AddPatient({ onClose, onAddPatient }) {
+  const [name, setName] = useState("");
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
+  const [gordura, setGordura] = useState("");
+  const [imc, setImc] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (isNaN(peso)){
-            window.alert("PESO INVÁLISO!\nPor favor, insira um número válido. O número pode ser decimal.");
-        }
-        else if(isNaN(altura)){
-            window.alert("ALTURA INVÁLIDA\nPor favor, insira um número válido. O número pode ser decimal.");
-        }
-        else if (gordura !== "" && isNaN(gordura)){
-            window.alert("% GORDURA INVÁLIDA\nPor favor, insira um número válido. O número pode ser decimal.");
-        }
-        else{
-            const pesoNum = parseFloat(peso);
-            const alturaNum = parseFloat(altura);
-            const calculaImc = pesoNum / (alturaNum * alturaNum);
-            const imcInformado = calculaImc.toFixed(2);
-            setImc(calculaImc);
-            const newPatient = { name, peso: pesoNum, altura: alturaNum, gordura, imc: imcInformado};
-                onAddPatient(newPatient);
-                onClose();
-        }
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isNaN(peso)) {
+      window.alert(
+        "PESO INVÁLISO!\nPor favor, insira um número válido. O número pode ser decimal."
+      );
+    } else if (isNaN(altura)) {
+      window.alert(
+        "ALTURA INVÁLIDA\nPor favor, insira um número válido. O número pode ser decimal."
+      );
+    } else if (gordura !== "" && isNaN(gordura)) {
+      window.alert(
+        "% GORDURA INVÁLIDA\nPor favor, insira um número válido. O número pode ser decimal."
+      );
+    } else {
+      const pesoNum = parseFloat(peso);
+      const alturaNum = parseFloat(altura);
+      const calculaImc = pesoNum / (alturaNum * alturaNum);
+      const imcInformado = calculaImc.toFixed(2);
+      setImc(calculaImc);
+      const newPatient = {
+        name,
+        peso: pesoNum,
+        altura: alturaNum,
+        gordura,
+        imc: imcInformado,
+      };
+      onAddPatient(newPatient);
+      onClose();
+    }
+  };
 
-    return(
-        <div className='add-patient-content'>
-                <div className='svg-content-close'>
-                <button className='btn-closee' onClick={onClose}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
-                </button>
-
-                </div>
-                <h1>Adicionar novo paciente</h1>
-                <form onSubmit={handleSubmit} className="form-add-patient" name="formulario">
-                    <div className="input-dados">
-                        <div className="col-md-12 bg-white">
-                        <label className="label-add-patient long-label bg-white" htmlFor="nome">
-                            Nome*</label>
-                            <input type="text" name="nome" id="nome" required onChange={(e) => setName(e.target.value)}/>
-                            </div>
-                        <div className="input-informacao col-md-12">
-                            <div className="bg-white">
-                                <label className="label-add-patient bg-white" htmlFor="peso" >
-                                    Peso*
-                                </label>
-                                <input type="text" name="peso" id="peso" required onChange={(e) => setPeso(e.target.value)}/>
-                            </div>
-                            <div className="bg-white">
-                            <label className="label-add-patient bg-white" htmlFor="altura" required>
-                                Altura*
-                                
-                            </label>
-                            <input type="text" name="altura" id="altura" required onChange={(e) => setAltura(e.target.value)}/>
-                            </div>
-                        </div>
-                        <div className="col-md-12 bg-white">
-                        <label className="label-add-patient long-label label-last-information bg-white" htmlFor="por-gordura">
-                            % de Gordura
-                        </label>
-                        <input type="text" name="por-gordura" id="por-gordura" onChange={(e) => setGordura(e.target.value)}/>
-                        </div>
-                </div>
-                <div className="campo-botao">
-                    <label className="label-add-patient" htmlFor="btnAdicionar">
-                        <input type="submit" value="Adicionar" id="btnAdicionar" />
-                    </label>
-                    {/* <label className="label-add-patient" htmlFor="btnGetCDCollection">
-                        <input type="submit" value="Get my CD Collection" id="btnGetCDCollection" />
-                    </label> */}
-                </div>
-            </form>
+  return (
+    <div className="add-patient-content">
+      <div className="svg-content-close">
+        <button className="btn-closee" onClick={onClose}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+          </svg>
+        </button>
+      </div>
+      <h1>Adicionar novo paciente</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="form-add-patient"
+        name="formulario"
+      >
+        <div className="input-dados">
+          <div className="col-md-12 bg-white">
+            <label
+              className="label-add-patient long-label bg-white"
+              htmlFor="nome"
+            >
+              Nome*
+            </label>
+            <input
+              type="text"
+              name="nome"
+              id="nome"
+              required
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="input-informacao col-md-12">
+            <div className="bg-white">
+              <label className="label-add-patient bg-white" htmlFor="peso">
+                Peso*
+              </label>
+              <input
+                type="text"
+                name="peso"
+                id="peso"
+                required
+                onChange={(e) => setPeso(e.target.value)}
+              />
             </div>
-    )
-};
+            <div className="bg-white">
+              <label
+                className="label-add-patient bg-white"
+                htmlFor="altura"
+                required
+              >
+                Altura*
+              </label>
+              <input
+                type="text"
+                name="altura"
+                id="altura"
+                required
+                onChange={(e) => setAltura(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="col-md-12 bg-white">
+            <label
+              className="label-add-patient long-label label-last-information bg-white"
+              htmlFor="por-gordura"
+            >
+              % de Gordura
+            </label>
+            <input
+              type="text"
+              name="por-gordura"
+              id="por-gordura"
+              onChange={(e) => setGordura(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="campo-botao">
+          <label className="label-add-patient" htmlFor="btnAdicionar">
+            <input type="submit" value="Adicionar" id="btnAdicionar" />
+          </label>
+        </div>
+      </form>
+    </div>
+  );
+}
 
 export default AddPatient;
--- src/components/main.js
+++ src/components/main.js
@@ -1,97 +1,114 @@
-import React, { useState } from 'react';
-import AddPatient from './addPatient.js';
-import TablePatient from './tablePatient.js';
-import '../css/global.css';
-import '../css/main.css';
+import React, { useState } from "react";
+import AddPatient from "./addPatient.js";
+import TablePatient from "./tablePatient.js";
+import "../css/global.css";
+import "../css/main.css";
 
 function Main() {
-    const [showAddPatient, setShowAddPatient] = useState(false);
-    const [patientsData, setPatientsData] = useState([]);
+  const [showAddPatient, setShowAddPatient] = useState(false);
+  const [patientsData, setPatientsData] = useState([]);
 
-    // Função para abrir o modal de adicionar paciente
-    const handleAddPatientClick = () => {
-        setShowAddPatient(true);
-    };
+  // Função para abrir o modal de adicionar paciente
+  const handleAddPatientClick = () => {
+    setShowAddPatient(true);
+  };
 
-    // Função para fechar o modal
-    const handleCloseModal = () => {
-        setShowAddPatient(false);
-    };
+  // Função para fechar o modal
+  const handleCloseModal = () => {
+    setShowAddPatient(false);
+  };
 
-    //Adicionar um paciente
-    const addPatient = (newPatient) => {
-        setPatientsData((prevData) => [...prevData, newPatient]);  // Adiciona o novo paciente
-    };
+  //Adicionar um paciente
+  const addPatient = (newPatient) => {
+    setPatientsData((prevData) => [...prevData, newPatient]); // Adiciona o novo paciente
+  };
 
-     // Função para buscar os dados XML
-     const buscaDadosXML = async () => {
-        const resposta = await fetch('./cd_pacientes.xml');
-        const texto = await resposta.text();
-        return new window.DOMParser().parseFromString(texto, 'application/xml');
-    };
+  // Função para buscar os dados XML
+  const buscaDadosXML = async () => {
+    const resposta = await fetch("/xml/cd_pacientes.xml");
 
-    // Função para processar os dados XML e atualizar a tabela
-    const processXMLData = (xmlDoc) => {
-        const cdItems = xmlDoc.getElementsByTagName("CD");
+    const texto = await resposta.text();
+    return new window.DOMParser().parseFromString(texto, "application/xml");
+  };
 
-        if (cdItems.length === 0) {
-            console.error("Nenhum dado encontrado no XML.");
-            return;
-        }
+  // Função para processar os dados XML e atualizar a tabela
+  const processXMLData = (xmlDoc) => {
+    const cdItems = xmlDoc.getElementsByTagName("CD");
 
-        const data = [];
-        
-        for (let i = 0; i < cdItems.length; i++) {
-            const name = cdItems[i].getElementsByTagName("NAME")[0]?.childNodes[0]?.nodeValue || "N/A";
-            const peso = cdItems[i].getElementsByTagName("PESO")[0]?.childNodes[0]?.nodeValue || "N/A";
-            const altura = cdItems[i].getElementsByTagName("ALTURA")[0]?.childNodes[0]?.nodeValue || "N/A";
-            const gordura = cdItems[i].getElementsByTagName("GORDURA")[0]?.childNodes[0]?.nodeValue || "N/A";
-            const imc = cdItems[i].getElementsByTagName("IMC")[0]?.childNodes[0]?.nodeValue || "N/A";
-
-            data.push({ name, peso, altura, gordura, imc });
-        }
-
-        // setPatientsData(data); // Atualiza os dados da tabela
-        setPatientsData((prevData) => [...prevData, ...data]); // Adiciona os novos dados ao estado existente
-        console.log(data); // Para verificação
-    };
-
-   
-
-    const handleButtonClick = () => {
-        //passando os dados para a função
-        buscaDadosXML().then(xmlData => {
-            processXMLData(xmlData);
-        })
+    if (cdItems.length === 0) {
+      console.error("Nenhum dado encontrado no XML.");
+      return;
     }
 
-    return (
-        <main>
-            <section className="first-section">
-                <h1>Meus pacientes</h1>
-                <div className='buttons-content'>
-                    <button className="btn-add" id="btnGetCDCollection" onClick={handleButtonClick}>
-                        Get my CD Collection
-                    </button>
-                    <button className="btn-add" id="btnAddPatient" onClick={handleAddPatientClick}>
-                        Adicionar paciente
-                    </button>
-                </div>
-            </section>
+    const data = [];
 
-            {/* Modal para adicionar paciente */}
-            {showAddPatient && (
-                <div className="modal-overlayy" onClick={handleCloseModal}>
-                    <div className="modal-contentt" onClick={(e) => e.stopPropagation()}>
-                        <AddPatient onClose={handleCloseModal} onAddPatient={addPatient}/>
-                    </div>
-                </div>
-            )}
+    for (let i = 0; i < cdItems.length; i++) {
+      const name =
+        cdItems[i].getElementsByTagName("NAME")[0]?.childNodes[0]?.nodeValue ||
+        "N/A";
+      const peso =
+        cdItems[i].getElementsByTagName("PESO")[0]?.childNodes[0]?.nodeValue ||
+        "N/A";
+      const altura =
+        cdItems[i].getElementsByTagName("ALTURA")[0]?.childNodes[0]
+          ?.nodeValue || "N/A";
+      const gordura =
+        cdItems[i].getElementsByTagName("GORDURA")[0]?.childNodes[0]
+          ?.nodeValue || "N/A";
+      const imc =
+        cdItems[i].getElementsByTagName("IMC")[0]?.childNodes[0]?.nodeValue ||
+        "N/A";
 
-            {/* Renderizando a tabela de pacientes */}
-            <TablePatient data={patientsData} />
-        </main>
-    );
+      data.push({ name, peso, altura, gordura, imc });
+    }
+
+    // setPatientsData(data); // Atualiza os dados da tabela
+    setPatientsData((prevData) => [...prevData, ...data]); // Adiciona os novos dados ao estado existente
+    console.log(data); // Para verificação
+  };
+
+  const handleButtonClick = () => {
+    //passando os dados para a função
+    buscaDadosXML().then((xmlData) => {
+      processXMLData(xmlData);
+    });
+  };
+
+  return (
+    <main>
+      <section className="first-section">
+        <h1>Meus pacientes</h1>
+        <div className="buttons-content">
+          <button
+            className="btn-add"
+            id="btnGetCDCollection"
+            onClick={handleButtonClick}
+          >
+            Get my CD Collection
+          </button>
+          <button
+            className="btn-add"
+            id="btnAddPatient"
+            onClick={handleAddPatientClick}
+          >
+            Adicionar paciente
+          </button>
+        </div>
+      </section>
+
+      {/* Modal para adicionar paciente */}
+      {showAddPatient && (
+        <div className="modal-overlayy" onClick={handleCloseModal}>
+          <div className="modal-contentt" onClick={(e) => e.stopPropagation()}>
+            <AddPatient onClose={handleCloseModal} onAddPatient={addPatient} />
+          </div>
+        </div>
+      )}
+
+      {/* Renderizando a tabela de pacientes */}
+      <TablePatient data={patientsData} />
+    </main>
+  );
 }
 
 export default Main;
